913d643f15bb09a8f178e5a58070157970f966f3
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-02-23 21:48:24 +0300

    1.1

---
+++ part1/courseInfo/src/App.jsx
@@ -0,0 +1,32 @@
+
+
+const Header = (props) => {
+  return(<h1>{props.course}</h1>)
+}
+
+const Content = (props) => {
+  return(<p>{props.part} {props.exercise}</p>)
+}
+
+const Total = (props) => {
+  return(<p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>);
+}
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React'
+  const ex1 = 10
+  const part2 = 'Using props to pass data'
+  const ex2 = 7
+  const part3 = 'State of a component'
+  const ex3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exercise={ex1}/>
+      <Total />
+    </div>
+  );
+}
+
+export default App;
