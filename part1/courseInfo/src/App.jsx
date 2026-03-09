002d261fed039406994ff486337b8647d6084a02
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-03-09 16:06:31 +0300

    1.3

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,6 +1,7 @@
 
 
 const Header = (props) => {
+  console.log(props)
   return(<h1>{props.course}</h1>)
 }
 
@@ -23,12 +24,18 @@ const Total = (props) => {
 }
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React'
-  const ex1 = 10
-  const part2 = 'Using props to pass data'
-  const ex2 = 7
-  const part3 = 'State of a component'
-  const ex3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
