0aa6be192f2f06e3731af93cae759d72f1b166f4
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-06-15 19:55:36 +0300

    1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,18 +8,22 @@ const Button = (props) => (
   <button onClick={props.onClick}>{props.text}</button>
 )
 
+const StatisticLine = (props) => {
+  return (<p>{props.text} {props.value}</p>)
+}
+
 const Statistics = (props) => {
   if (!props.good && !props.neutral && !props.bad){
     return(<p>No feedback given</p>)
   }
   return(
   <div>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
-    <p>all {props.total}</p>
-    <p>average {(props.good - props.bad) / props.total}</p>
-    <p>positive {(props.good / props.total) * 100} %</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={props.total} />
+      <StatisticLine text="average" value={(props.good - props.bad) / props.total} />
+      <StatisticLine text="positive" value={(props.good / props.total) * 100} />
   </div>)
 }
 
