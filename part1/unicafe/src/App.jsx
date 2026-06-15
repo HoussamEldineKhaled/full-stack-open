919490b6809d906b6277d01c5c9988c1d56033c1
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-06-15 19:31:54 +0300

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,47 +8,38 @@ const Button = (props) => (
   <button onClick={props.onClick}>{props.text}</button>
 )
 
-const Statistics = (props) => (
-  <p></p>
-)
+const Statistics = (props) => {
+  return(
+  <div>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+    <p>all {props.total}</p>
+    <p>average {(props.good - props.bad) / props.total}</p>
+    <p>positive {(props.good / props.total) * 100} %</p>
+  </div>)
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const incrementGood = newValue => {
-    console.log("current value", newValue)
-    setGood(newValue)
-  }
-
-    const incrementNeutral = newValue => {
-    console.log("current value", newValue)
-    setNeutral(newValue)
-  }
-
-    const incrementBad = newValue => {
-    console.log("current value", newValue)
-    setBad(newValue)
-  }
   const total = good + bad + neutral
+
 
   return (
   <div>
     <h1>give feedback</h1>
     <div>
-      <Button onClick={() => incrementGood(good + 1)} text="good"/>
-      <Button onClick={() => incrementNeutral(neutral + 1)} text="neutral"/>
-      <Button onClick={() => incrementBad(bad + 1)} text="bad"/>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/>
     </div>
 
     <h1>statistics</h1>
     <br/>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {total}</p>
-    <p>average {(good - bad) / total}</p>
-    <p>positive {(good / total) * 100} %</p>
+    <Statistics good = {good} bad = {bad} neutral = {neutral} total = {total}/>
   </div>)
 }
 
