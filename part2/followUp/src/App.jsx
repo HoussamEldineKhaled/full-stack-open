b826d9a411287083c2133dfb8a5446d5f8851570
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-07-03 21:32:08 +0300

    practice of section 2

---
+++ part2/followUp/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+
+
+function App(props) {
+  const {notes} = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <li>{note.content}</li>)}
+      </ul>
+    </div>
+    )
+}
+
+export default App
