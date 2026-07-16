7848291036adb89b477e12d33259ca2cc5d2136d
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-07-16 17:28:05 +0300

    part 2.6

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -3,8 +3,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [persons, setPersons] = useState([{name: 'Arlo Helas'}])
+  const [persons, setPersons] = useState([{id: 1, name: 'Arlo Helas'}])
   const [newNames, setNewNames] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewNames(event.target.value)
+  }
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      id: persons.length + 1,
+      name: newNames
+    }
+    setPersons(persons.concat(newPerson))
+    console.log("Added")
+  }
 
 
   return (
@@ -12,14 +26,16 @@ function App() {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name : <input/> 
+          name : <input value={newNames} onChange={handleNameChange}/> 
         </div>
         <div>
-          <button type='submit'>Add</button>
+          <button type='submit' onClick={addName}>Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-
+      <div>
+      <ul>{persons.map(person => <p key={person.id}>{person.name}</p>)}</ul>
+      </div>
     </div>
   )
 }
