478153e19efdeb866a8926f68a1b29cf152818e8
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-07-26 18:56:17 +0300

    axios and useEffect

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [persons, setPersons] = useState([
-    {id: 1, name: 'Arlo Helas', number: '70-246-578'}, 
-    {id: 2, name: 'Arturo Yonas', number: '80-987-333'}, 
-    {id: 3, name: 'Alberto Margarette', number: '90-566-3456'}])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredPersons, setFilteredPersons] = useState([])
 
-
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log("Promise fulfilled")
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>
