7eb877bd28585265c22452912e08840a8a2f81de
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-07-26 17:59:53 +0300

    beginning exercise 2.11

--- part2/followUp/src/App.jsx
+++ part2/followUp/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import Note from './components/note'
+import axios from 'axios'
 
 
-function App(props) {
-  const [notes, setNotes] = useState(props.notes)
+function App() {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log('Promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
 
   const addNote = (event) => {
     event.preventDefault()
